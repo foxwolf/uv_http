4373f3289ddae67d12e71eb7a8cd028bfbd6aae6
yangmin <yangmin@flywall.eu>
2023-12-12 16:17:40 +0800

    test libuv library

--- cmake/define.cmake
+++ cmake/define.cmake
@@ -1,4 +1,16 @@
 cmake_minimum_required(VERSION 3.10.0)
 
-set(LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/build/lib)
+set(LIBRARY_OUTPUT_PATH ${PROJECT_BINARY_DIR}/build/lib)
 set(EXECUTABLE_OUTPUT_PATH ${PROJECT_BINARY_DIR}/build/bin)
+
+option(
+    BUILD_WITH_UV
+    "If build with libuv"
+    ON
+)
+
+# disable all assert
+#IF ((${DISABLE_ASSERT} MATCHES "true") OR (${DISABLE_ASSERTS} MATCHES "true"))
+ADD_DEFINITIONS(-DDISABLE_ASSERT)
+#MESSAGE(STATUS "Disable all asserts")
+#ENDIF()
--- example/src/main.c
+++ example/src/main.c
@@ -1,8 +1,13 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
+
+#include "uv.h"
 
 int main(int argc, char **argv) {
-    printf("cmake ~\n");
+  char *version;
 
-    return 0;
+  version = (char *)uv_version_string();
+
+  printf("libuv version is %s\n", version);
+  return 0;
 }
